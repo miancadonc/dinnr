cec17371fcafb178b02f3b1cefb1b0037ba0e45e
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-02-21 07:46:40 -0500

    fixed environment to work with database

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
-require 'bundler'
+ENV['SINATRA_ENV'] ||= "development"
 
-Bundler.require
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3"
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-# database => "db/development.sqlite"
-# )
 
 require_all 'app'
