f640aadfc3c9c01a9df9d34eb7049df4fb9bee92
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-03-02 05:47:13 -0500

    simplify models and tabels

--- db/migrate/20200227094314_create_recipes.rb
+++ db/migrate/20200227094314_create_recipes.rb
@@ -3,6 +3,8 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
     create_table :recipes do |t|
       t.string :name
       t.string :url
+      t.integer :rating
+      t.integer :ease_of_preparation
     end
   end
 end
