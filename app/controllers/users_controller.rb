ebaac3d869796f0c56b1b7ca3b6b99c2d3f74d0d
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-03-02 06:53:58 -0500

    create sign up forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+
     get '/signup' do
         if !session[:user_id]
           erb :'users/new'
         else
-          redirect to '/recipe_books'
+          "You're already signed in!"
         end
+    end
+
+    post '/signup' do
+      if params[:username] == "" || params[:password] == ""
+        @signup_error = "Please enter a username and a password:"
+        redirect to '/signup'
+      else
+        @user = User.create(:username => params[:username], :password => params[:password])
+        session[:user_id] = @user.id
+        redirect '/'
       end
+    end
+
 end
