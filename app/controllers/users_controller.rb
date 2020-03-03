8d2e056c9e6e9cf9ad6b394d01d6261e911aa834
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-03-03 09:12:32 -0500

    redirect successful signup to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       else
         @user = User.create(:username => params[:username], :password => params[:password])
         session[:user_id] = @user.id
-        redirect '/'
+        redirect "/users/#{current_user.id}/show"
       end
     end
 
