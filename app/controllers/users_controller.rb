ad24299c85ea643bccd4a6bae990f1abe429f164
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-03-03 05:16:56 -0500

    create login views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     get '/signup' do
         if !session[:user_id]
-          erb :'users/new'
+          erb :'users/signup'
         else
           "You're already signed in!"
         end
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
 
     post '/signup' do
       if params[:username] == "" || params[:password] == ""
-        @signup_error = "Please enter a username and a password:"
-        redirect to '/signup'
+        @signup_error = "Sorry, you didn't enter both a username and a password. Please enter a username and a password:"
+        erb :'users/signup'
       else
         @user = User.create(:username => params[:username], :password => params[:password])
         session[:user_id] = @user.id
@@ -20,7 +20,32 @@ class UsersController < ApplicationController
     end
 
     get '/login' do
-      
+      if !logged_in?
+        erb :'users/login'
+      else
+        "You're already signed in!"
+      end
+    end
+
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/users/#{user.id}/show"
+      else
+        @signup_error = "Sorry, user not found. Please sign up here:"
+        erb :'users/signup'
+      end
+    end
+
+    get '/users/:id/show' do
+      if logged_in?
+        @user = current_user
+        erb :'users/show'
+      else
+        @login_error = "User not found. Please log in or sign up:"
+        erb :index
+      end
     end
 
 end
