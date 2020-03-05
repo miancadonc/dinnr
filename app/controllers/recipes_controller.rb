803e049586f171408f8fc13b76f34aeaad96ec31
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-03-05 14:43:27 -0500

    clean up routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,38 +1,38 @@
 class RecipesController < ApplicationController
 
-    get '/recipes' do
-        'recipes index'
-    end
+    # get '/recipes' do
+    #     'recipes index'
+    # end
 
-    get '/recipes/new' do
-        erb :'recipes/new'
-    end
+    # get '/recipes/new' do
+    #     erb :'recipes/new'
+    # end
 
     post '/recipes' do
         if params[:name] == '' || params[:url_location] == ''
-            erb :'recipes/new'
+            redirect "/users/#{current_user.id}"
         else
             recipe = Recipe.create(params)
          
-            redirect "/users/#{current_user.id}"
+            redirect "/recipe-books/#{recipe.recipe_book_id}"
         end
     end
 
-    get '/recipes/:id' do
-        'load show file with @id'
-    end
+    # get '/recipes/:id' do
+    #     'load show file with @id'
+    # end
 
-    get '/recipes/:id/edit' do
-        'load edit form, save updated @id'
-    end
+    # get '/recipes/:id/edit' do
+    #     'load edit form, save updated @id'
+    # end
 
-    patch '/recipes/:id' do
-        'in controller, update attributes. pull up index (redirect to /recipes)'
-    end
+    # patch '/recipes/:id' do
+    #     'in controller, update attributes. pull up index (redirect to /recipes)'
+    # end
 
     delete '/recipes/:id' do
-        @recipe = Recipe.find(params[:id])
-        @recipe.destroy
-        redirect "/users/#{current_user.id}"
+        recipe = Recipe.find(params[:id])
+        recipe.destroy
+        redirect "/recipe-books/#{recipe.recipe_book_id}"
     end
 end
